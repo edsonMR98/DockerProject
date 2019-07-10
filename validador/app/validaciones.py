64d06b725c3b13a252935f7db4310ad8359dec6c
Edson Moreno <edsonmorenoruiz@gmail.com>
2019-07-10 16:32:04 -0500

    Validaciones

--- validador/app/validaciones.py
+++ validador/app/validaciones.py
@@ -1,23 +1,36 @@
 import json
 
-# m: payload, message from mqtt
-# r: archivo rangos.json
 def validar(m, r):
-    data = toDict(m)    # data recibida de mqtt en tipo dict
-    rangos = r          # rangos para validar la data
+    """ Valida el mensaje recibido por MQTT.\n
+        m: Mensaje recibido por MQTT (payload)\n
+        r: Json usado para tomar las referencias de las mediciones\n
+        return: Mensaje (json) validado """
+    data = toDict(m)
+    rangos = r
 
-    for d in data["mediciones"]:
-        print("Id: ", d["idParametro"])
-        if (d["valor"] >= rangos["limites"][d["idParametro"]]["min"]) and (d["valor"] <= rangos["limites"][d["idParametro"]]["max"]):
-            print("Estado: Correcto")
-        else:
-            print("Estado: Incorrecto")
-        print("")
+    banderaLimpieza(data, rangos)
+    print(data)
     
-    
-    
-
-# Convertir un dato tipo bytes to dict
 def toDict(fileBytes):
+    """ Convertir un dato tipo bytes to dict.\n
+        fileBytes: mensaje tipo bytes a convertir """
     fileStr = str(fileBytes.decode("utf-8"))
     return json.loads(fileStr)
+
+def banderaLimpieza(data, rangos):
+    """ Agrega la bandera de limpieza a cada medicion.\n
+    data: Mensaje\n
+    rangos: Json usado para tomar las referencias de las mediciones"""
+    for d in data["mediciones"]:        
+        if (d["valor"] == None):
+            # Estado: Dato que no está disponible ND
+            d["bLimpieza"] = "ND"
+        elif (d["valor"] >= rangos["limites"][d["idParametro"]]["min"]) and (d["valor"] <= rangos["limites"][d["idParametro"]]["max"]) and (d["valor"] >= 0):
+            # Estado: Valido VA
+            d["bLimpieza"] = "VA"
+        elif (d["valor"] >= rangos["limites"][d["idParametro"]]["min"]) and (d["valor"] <= rangos["limites"][d["idParametro"]]["max"]) and (d["valor"] < 0):
+            # Estado: Dato válido igualado al límite de detección o a cero VZ
+            d["bLimpieza"] = "VZ"
+        elif (d["valor"] < rangos["limites"][d["idParametro"]]["min"]) or (d["valor"] > rangos["limites"][d["idParametro"]]["max"]):
+            # Estado: Dato inválido por rango de operación IR
+            d["bLimpieza"] = "IR"
