f67bd7cd23bca10ec983c7f95a3adecee1fb9cf3
LeitzaJmz <yoshiaa98@gmail.com>
2019-07-11 16:25:26 -0500

    cambio del nombre de la funcion de test

--- sensorPublish/app/app.py
+++ sensorPublish/app/app.py
@@ -8,7 +8,8 @@ mqtthost = '127.0.0.1' ##broker mqtt ip
 mqttport = 1883 #default mqtt port
 
 #test function to publish data in the topic "wsnJson"
-def reaDataWsn():
+#In this space will be the function that receives data from the movil app and publish it in the wsnJson topic
+def testFunction():
     for x in range(5):
         wsnData = '{"idSensor":"1525","mediciones":{"idParametro":55,"valor":"36"},"dateTime":"10/07/2019"}' #jsonPrueba
         client.publish("wsnJson", wsnData) #wsnJson is the name of the topic where wsnData will be publish
@@ -21,5 +22,5 @@ def on_log(client, userdata, level, buf):
 client = mqtt.Client()
 client.on_log = on_log
 client.connect(mqtthost, mqttport) 
-reaDataWsn()
+testFunction()
 client.loop_forever()
