7143644f470610171c548651c812bbb9eeb3bf1e
Edson Moreno <edsonmorenoruiz@gmail.com>
2019-11-26 15:05:14 -0600

    Comentarios agregados

--- sensornetwork/app/app.py
+++ sensornetwork/app/app.py
@@ -3,30 +3,33 @@ import json
 import time
 import datetime
 import csv
+# Se importan las librerias necesarias
 
 # Visualiza los registros que se realizan
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
 
-# Establece conexion al broker MQTT, Subscribirse en el topico especificado (sensores2)
+# Establece conexion al broker MQTT
+# Subscripcion en el topico especificado (sensores2)
 def on_connect(client, userdata, flags, rc):
     #connected
     print("Conectado")
     client.subscribe(topic='sensores2', qos=2)
     
-# Se define y crea un cliente del broker MQTT 
+# Se define y crea un cliente del broker MQTT, usando la libreria mqtt
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_log = on_log
 client.connect('mqttserver', 1883)
 
-# Usa el archivo dataset.csv como csvFile
+# Usa el archivo dataset (./dataset.csv) como csvFile
 # se lee el archivo csv y se reestructura para ser publicado en el broker mqtt
 with open('dataset.csv') as csvFile:
-    csvReader = csv.DictReader(csvFile) # Lee el archivo CSV y lo convierte en un Dict
+    csvReader = csv.DictReader(csvFile) # Lee el archivo CSV y lo convierte en un Dict de python
     tempHour = ''
+    # Se recorre el dict previamente creado
     for csvRow in csvReader:
-        data = {} # Se crea un dict vacio
+        data = {} # Se crea un Dict vacio
         data['dateTime'] = csvRow['datetime']   # Se crea la columna dateTime y se le agrega el valor que esta en el archivo CSV
         data['idStation'] = csvRow['clave_estacion']    # Se crea la columna isStations y se le agrega el valor que esta en el archivo CSV
         data['mediciones'] = [] # Se crea la columna mediciones vacia
